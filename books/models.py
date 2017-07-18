cac7acdda0ca8fb83be1bba613c8305177440b14
PPK17 <ppk17y@gmail.com>
2017-07-17 23:44:07 -0500

    Model Author created

--- books/models.py
+++ books/models.py
@@ -4,10 +4,16 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=150)
-    author = models.CharField(max_length=100,help_text='Nombre del autor')
+    authors = models.ManyToManyField("Author", related_name="books")
     review = models.TextField(blank=True,null=True)
     date_review = models.DateTimeField(blank=True,null=True)
     is_favourite = models.BooleanField(default=False,verbose_name='Favorite')
 
     def __str__(self):
         return self.title
+
+class Author(models.Model):
+    name = models.CharField(max_length=150,help_text="Ingrese nombre del autor")
+
+    def __str__(self):
+        return self.name
