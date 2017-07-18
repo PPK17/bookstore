677df3527b7c7106647154511fb79c1b4e12ea91
PPK17 <ppk17y@gmail.com>
2017-07-17 22:13:08 -0500

    Modificado Model a Book, creado super usuario, y agregar el Model a Admin

--- books/models.py
+++ books/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class Books(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=150)
-    author = models.CharField(max_length=100)
+    author = models.CharField(max_length=100,help_text='Nombre del autor')
     review = models.TextField(blank=True,null=True)
     date_review = models.DateTimeField(blank=True,null=True)
-    is_favourite = models.BooleanField(default=False)
+    is_favourite = models.BooleanField(default=False,verbose_name='Favorite')
+
+    def __str__(self):
+        return self.title
