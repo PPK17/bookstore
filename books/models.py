6974504dcead8788c5fd27482d60167e243c4365
PPK17 <ppk17y@gmail.com>
2017-07-17 21:45:24 -0500

    Created Books Model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Books(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.CharField(max_length=100)
+    review = models.TextField(blank=True,null=True)
+    date_review = models.DateTimeField(blank=True,null=True)
+    is_favourite = models.BooleanField(default=False)
