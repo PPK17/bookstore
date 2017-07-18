9d67d8ac4582c4ab6186bae7d0d1cd6f4f8784ad
PPK17 <ppk17y@gmail.com>
2017-07-17 23:55:27 -0500

    Create manytomany relations Author-Book

--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
 
@@ -10,7 +11,17 @@ class Book(models.Model):
     is_favourite = models.BooleanField(default=False,verbose_name='Favorite')
 
     def __str__(self):
-        return self.title
+        return "{} by {}".format(self.title,self.list_authors())
+
+    def list_authors(self):
+        return ", ".join([author.name for author in self.authors.all()])
+
+    def save(self, *args, **kwargs):
+        if (self.review and self.date_review is None):
+            self.date_review = now()
+
+        super(Book, self).save(*args, **kwargs)
+
 
 class Author(models.Model):
     name = models.CharField(max_length=150,help_text="Ingrese nombre del autor")
